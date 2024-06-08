fea09abd138eac2b54e8a82228e0dfc237e4149e
Ankur Maurya <31955983+ankurjbs@users.noreply.github.com>
2024-06-08 23:57:16 +0530

    Creating header and quiz component

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import quizlogo from '../assets/quiz-logo.png';
+export default function Header(){
+    return <header>
+        <img src={quizlogo} alt='Quiz Logo'/>
+        <h1>React Quiz</h1>
+    </header>
+}
