9dedf770dc73bf48b755115ee19bd0f9644fd9aa
ankurjbs <ankurjbs@gmail.com>
2024-06-12 16:50:03 +0530

    Adding questions and selected answer is done

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
+import QUESTIONS from "../questions";
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
 
-export default function Quiz(){
-    const [activeQuestionIndex,setActiveQuestionIndex] = useState(0);
-    const [userAnswers,setUserAnswers] = useState();
-    return <p>Currently Active Questions</p>
+  // initially activeQuestionIndex will be zero , bcz initially useState is empty
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((preValue) => {
+      return [...preValue, selectedAnswer];
+    });
+  }
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
