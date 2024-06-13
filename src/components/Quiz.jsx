dc402b2e388e11485fbd39d0722eaeebbdde7500
ankurjbs <ankurjbs@gmail.com>
2024-06-13 17:18:18 +0530

    Higligted selected answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,23 +3,46 @@ import QUESTIONS from "../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer";
 
-const TIMER = 5000;
+const TIMER = 15000;
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
   // initially activeQuestionIndex will be zero , bcz initially useState is empty
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback( function handleSelectAnswer(selectedAnswer) {
-    setUserAnswers((preValue) => {
-      return [...preValue, selectedAnswer];
-    });
-  },[]);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      // to set the one option is selected
+      setAnswerState("answered");
+      setUserAnswers((preValue) => {
+        return [...preValue, selectedAnswer];
+      });
 
-  const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null),[handleSelectAnswer])
+      // after 1 second to check answer is right or wrong
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+      }, 1000);
+
+      setTimeout(() => {
+        setAnswerState("");
+      }, 2000);
+    },
+    [activeQuestionIndex]
+  );
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
     return (
@@ -37,19 +60,35 @@ export default function Quiz() {
       <div id="question">
         {/* key prop can be added to any element and any compo. Before we know that it is using when outputting the list data. Bcz this key helps React identify those diff list items. And it helps react to manage that list efficiently. But key prop also has another purpose , when it changes React will destroy old component instance and create new one. So it will unmount and remount basically. here activeQuestionsIndex are changing so progress bar resetting   */}
         <QuestionTimer
-        key={activeQuestionIndex}
+          key={activeQuestionIndex}
           timeout={TIMER}
           onTimeOut={handleSkipAnswer}
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswers.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let cssClasses = "";
+            if (answerState == "answered" && isSelected) {
+              cssClasses = "selected";
+            }
+            if (
+              (answerState === "correct" || answerState === "wrong") &&
+              isSelected
+            ) {
+              cssClasses = answerState;
+            }
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClasses}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
